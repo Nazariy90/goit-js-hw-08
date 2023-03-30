4118d5d1e76042144d00d7e4870923d3e870a5c9
Nazariy90 <123194812+Nazariy90@users.noreply.github.com>
2023-03-30 18:14:26 +0000

    Deploying to gh-pages from @ Nazariy90/goit-js-hw-08@74c54b9dab1e2da0784e698a9040c4d3a866732a 🚀

---
+++ 03-feedback.3bd8356c.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("change",(function(e){}));
+//# sourceMappingURL=03-feedback.3bd8356c.js.map
---
+++ 03-feedback.c349c275.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("change",(e=>{}));
+//# sourceMappingURL=03-feedback.c349c275.js.map
