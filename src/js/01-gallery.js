eec69fdd30d33dc2961ae4ca3b0e45258b43a8d9
Nazariy90 <pinhvin123@gmail.com>
2023-03-30 19:22:05 +0300

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const photosMark = createGalleryPhotos(galleryItems);
+
+function createGalleryPhotos(photos) {
+  return photos
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image lazyload"
+      src="${preview}" 
+      data-source="${original}" 
+      alt="${description}"/></a></div>`;
+    })
+    .join('');
+}
+galleryContainer.insertAdjacentHTML('beforeend', photosMark);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  animationSpeed: 250,
+});
